d105459c926d2854068032c6ce505a17e0c3d4cd
DAW <DAW@DESKTOP-VGUR76P>
2023-11-06 16:53:01 +0100

    Commit inicial con código básico para la AppPrueba de java

---
+++ Aplicacion_de_prueba/src/aplicacion_de_prueba/Aplicacion_de_prueba.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplicacion_de_prueba;
+
+/**
+ *
+ * @author DAW
+ */
+public class Aplicacion_de_prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
